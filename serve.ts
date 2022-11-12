ba2cf8dad3f05f5d254007d6e96e42fa6cca1cfa
MarcosValdez <marcos.valdez333@hotmail.com>
2022-11-12 15:09:35 -0500

    prueba vercel 3

--- serve.ts
+++
@@ -1,8 +0,0 @@
-import { port } from './src/config';
-import app from './src/app';
-
-app
-  .listen(port, () => {
-    console.log(`http://localhost:${port}/api/v1`);
-  })
-  .on('error ', e => console.log(e));
